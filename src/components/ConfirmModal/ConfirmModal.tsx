f69cf8bd37b5c5d9d4b99890bd336547867f43f9
rocketmike12 <mg.korop@gmail.com>
2026-06-22 19:34:54 +0300

    ConfirmModal styles

--- src/components/ConfirmModal/ConfirmModal.tsx
+++ src/components/ConfirmModal/ConfirmModal.tsx
@@ -10,12 +10,14 @@ export const ConfirmModal = function () {
 			<div className={`${styles["backdrop"]} ${isOpen ? "" : styles["hidden"]}`}>
 				<div className={styles["modal"]}>
 					<p className={styles["modal__message"]}>{message}</p>
-					<button className={styles["modal__confirm"]} onClick={confirm}>
-						Confirm
-					</button>
-					<button className={styles["modal__cancel"]} onClick={cancel}>
-						Cancel
-					</button>
+					<div className={styles["modal__wrap"]}>
+						<button className={styles["modal__confirm"]} onClick={confirm}>
+							Confirm
+						</button>
+						<button className={styles["modal__cancel"]} onClick={cancel}>
+							Cancel
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
