dc160f6b2893e3c40673a1870cb087acf1008fc5
rocketmike12 <mg.korop@gmail.com>
2026-06-27 10:17:03 +0300

    placeholder analytics

--- src/pages/AnalyticsPage/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage/AnalyticsPage.tsx
@@ -4,6 +4,11 @@ export const AnalyticsPage = function () {
 	return (
 		<>
 			<Header />
+			<img
+				src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdW15M21henRiY2szOXVjNmlxMjV6NHRpeTk5YXUweWR3Ym55d250NCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8YeRRk1FPx7fzBCR0U/giphy.gif"
+				alt=""
+				style={{ height: "100%" }}
+			/>
 		</>
 	);
 };
